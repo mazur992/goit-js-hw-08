44f275b6d0aefcca5d415f0be059f85700578d66
Mazur Roman <superioritywork@gmail.com>
2023-03-15 20:58:34 +0100

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a rel="noopener noreferrer"  href=${original}><img loading="lazy" class="gallery__image" src=${preview} alt=${description
+        .split(' ')
+        .join('_')}></a></li>`
+  )
+  .join('');
+listEl.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
