4fb32fedcd38f8694ad31abea851b63d4b7f9308
Mazur Roman <superioritywork@gmail.com>
2023-03-17 07:00:30 +0100

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_VALUE = 'feedback-form-state';
+
+let formData = {};
+
+formEl.addEventListener(
+  'input',
+  throttle(event => {
+    formData[event.target.name] = event.target.value;
+
+    localStorage.setItem(STORAGE_VALUE, JSON.stringify(formData));
+  }, 500)
+);
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_VALUE);
+  formEl.reset();
+});
+
+function verifyLocalStorage() {
+  const storText = JSON.parse(localStorage.getItem(STORAGE_VALUE));
+
+  if (storText) {
+    formEl.email.value = storText.email;
+    formEl.message.value = storText.message;
+  }
+}
+
+verifyLocalStorage();
